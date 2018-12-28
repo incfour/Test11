6f33da51ca5056b20ef9ef0c2dde57daa50d9d7e
신무경 <incfour@golfzon.com>
2018-12-28 13:42:52 +0900

    Add project files.

---
+++ Source/Test11/MyActor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyActor.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+
+	
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString str = FString::Printf(TEXT("%d %f"), 10, -1.0f);
+
+	UE_LOG(LogTemp, Warning, TEXT("Your string: %s"), *str);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Test11/Test11GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test11GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST11_API ATest11GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
